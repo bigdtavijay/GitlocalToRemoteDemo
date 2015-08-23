a8be19ab9d0f4a2aae2bd65782c5a099da7b0e69
bigdtavijay <bigdatavijay@gmail.com>
2015-08-23 03:37:35 -0500

    After adding comment to test git status
    Change-Id: I5d4a1d2eee88dc45f57431ec125bfb776392eb28
    
    
    Change-Id: I5d4a1d2eee88dc45f57431ec125bfb776392eb28
    Signed-off-by: bigdtavijay <bigdatavijay@gmail.com>

--- src/com/demo/git/HelloWorld.java
+++ src/com/demo/git/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("First Git project");
 		System.out.println("Now it is in remote");
+		// Simple comment to test git status
 		
 	}
 
