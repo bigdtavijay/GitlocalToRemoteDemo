4bb6745738063b420a3970eaf199ba03a311674e
bigdtavijay <bigdatavijay@gmail.com>
2015-08-23 03:51:32 -0500

    Second file commiting
    Change-Id: I4e246cdb506f0ff4dd5cdb7b442606daa01960d0

---
+++ src/com/demo/git/Second.java
@@ -0,0 +1,16 @@
+package com.demo.git;
+
+public class Second {
+	
+	public String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
